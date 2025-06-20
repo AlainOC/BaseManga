155a37d5942f4cebd58eefddc36553e2053c9263
AlexisCelis13 <164053865+AlexisCelis13@users.noreply.github.com>
2025-06-20 16:03:03 -0600

    Cambios 9

--- MiMangaBot/Program.cs
+++ MiMangaBot/Program.cs
@@ -89,11 +89,9 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Habilitar Swagger SIEMPRE (desarrollo y producción)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
